240c9c80e109df8ed25bfdadf57130a7ced18c84
EC2 Default User <126998671+fuguita@users.noreply.github.com>
2023-04-04 03:32:15 +0000

    bookers2_3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,19 @@
 class BooksController < ApplicationController
-  
+
   def new
     @book = Book.new
-    
+
   end
-  
+
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     @book.save
     redirect_to book_path
-  end  
+  end
 
   def index
-  @books = Book.all  
+  @books = Book.all
   end
 
   def show
@@ -23,12 +24,18 @@ class BooksController < ApplicationController
     @book = Book.(params[:id])
   end
 
+  def update
+    @book = Book.find(params[:id])
+    @book.update
+    redirect_to book_path
+  end
+
   def destory
   end
-  
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,13 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @books = @user.books
 
   end
 
   def edit
     @user = User.find(params[:id])
-    redirect_to user_path
+  
   end
 
   def update
@@ -20,7 +21,7 @@ class UsersController < ApplicationController
 
 
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
