f442a29f26dca9e5ca368f616c79c823d16242d4
EC2 Default User <126998671+fuguita@users.noreply.github.com>
2023-04-05 16:10:17 +0000

    45

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -31,7 +31,11 @@ class BooksController < ApplicationController
     redirect_to book_path(@book)
   end
 
-  def destory
+  def destroy
+  book = Book.find(params[:id])
+  book.destroy
+  redirect_to books_path
+
   end
 
   private
