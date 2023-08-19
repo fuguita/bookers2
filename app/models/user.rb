f06aaf51e34687bf495fb5788102d8b5c825ceda
EC2 Default User <126998671+fuguita@users.noreply.github.com>
2023-08-19 14:25:16 +0000

    user_model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,16 @@ class User < ApplicationRecord
     has_many :likes, dependent: :destroy
     has_many :book_comments, dependent: :destroy
 
+    # 自分がフォローされる（被フォロー）側の関係性
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # 被フォロー関係を通じて参照→自分をフォローしている人
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
+  # 自分がフォローする（与フォロー）側の関係性
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # 与フォロー関係を通じて参照→自分がフォローしている人
+  has_many :followings, through: :relationships, source: :followed
+
     validates :name, length: { in: 2..20 }, uniqueness: true
     validates :introduction, length: { maximum: 50 }
 
