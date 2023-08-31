d810f6439140f2e4aef87ec66272adfdc0a2e8d9
EC2 Default User <126998671+fuguita@users.noreply.github.com>
2023-08-31 12:35:06 +0000

    k

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,12 +5,12 @@ class Book < ApplicationRecord
   has_many :book_comments, dependent: :destroy
 
   validates :title, presence: true
-  validates :body, presence: true, length: { maximum: 200 }
+  validates :body, presence: true, length: { maximum: 20 }
 
   def liked_by?(user)
     likes.exists?(user_id: user.id)
   end
-  
+
   def self.search_for(word, method)
     if method == 'perfect_match'
       Book.where(title: word)
@@ -18,9 +18,9 @@ class Book < ApplicationRecord
       Book.where('title LIKE ?', word+'%')
     elsif method == 'backward_match'
       Book.where('title LIKE ?', '%'+word)
-    else 
+    else
       Book.where('title LIKE ?', '%'+word+'%')
-    end  
-  end  
-  
+    end
+  end
+
 end
