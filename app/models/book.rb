184db1a647553258b340d59340323edfe754abb7
EC2 Default User <126998671+fuguita@users.noreply.github.com>
2023-08-31 12:35:42 +0000

    k

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,7 +5,7 @@ class Book < ApplicationRecord
   has_many :book_comments, dependent: :destroy
 
   validates :title, presence: true
-  validates :body, presence: true, length: { maximum: 20 }
+  validates :body, presence: true, length: { maximum: 200 }
 
   def liked_by?(user)
     likes.exists?(user_id: user.id)
