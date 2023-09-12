e471c476329cf08399246c385afa9f2b0287f274
EC2 Default User <126998671+fuguita@users.noreply.github.com>
2023-09-12 04:21:59 +0000

    book2

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,7 +7,7 @@ class Book < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
 
-  def liked_by?(use)
+  def liked_by?(user)
     likes.exists?(user_id: user.id)
   end
 
